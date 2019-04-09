aa87202643d0b05403bd00cfb3934124b2279bac
Enrico Pulvirenti <7995772+Harrypulvirenti@users.noreply.github.com>
2019-04-09 20:00:40 +0200

    Add some useful extensions

---
+++ ArchitectureSDK/src/main/java/com/hpdev/architecture/sdk/extensions/LifecycleOwnerExtensions.kt
@@ -0,0 +1,11 @@
+package com.hpdev.architecture.sdk.extensions
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+fun <T : Any> LifecycleOwner.observe(liveData: LiveData<T>, observer: (T) -> Unit) {
+    liveData.observe(this, Observer {
+        observer(it)
+    })
+}
---
+++ ArchitectureSDK/src/main/java/com/hpdev/architecture/sdk/extensions/ViewExtensions.kt
@@ -0,0 +1,27 @@
+package com.hpdev.architecture.sdk.extensions
+
+import android.view.View
+
+fun View.makeGone() {
+    visibility = View.GONE
+}
+
+fun View.makeVisible() {
+    visibility = View.VISIBLE
+}
+
+fun View.makeInvisible() {
+    visibility = View.INVISIBLE
+}
+
+fun View.isGone(): Boolean {
+    return visibility == View.GONE
+}
+
+fun View.isVisible(): Boolean {
+    return visibility == View.VISIBLE
+}
+
+fun View.isInvisible(): Boolean {
+    return visibility == View.INVISIBLE
+}
