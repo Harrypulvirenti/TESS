b9018045565a91574dfc916efecbc83a73f40a0f
Harry Pulvirenti <>
2020-04-05 14:03:53 +0200

    Add base structure for Login Feature

---
+++ features/login-feature/src/main/java/com/tess/features/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.tess.features.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
